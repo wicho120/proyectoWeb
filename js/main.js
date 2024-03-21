4238f9ac26f63746a3b06935451aad70a4dede45
Wicho120 <35779309+wicho120@users.noreply.github.com>
2024-03-21 05:26:52 -0500

    feat: :construction: Terminacion del header y todos los articulos responsive (falta footer)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-import {} from "./modules/app.js"
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
 
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
