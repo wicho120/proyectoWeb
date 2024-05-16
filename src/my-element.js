68ef5c2bfa1e78efa606280cd22d5455bae7b793
Wicho120 <35779309+wicho120@users.noreply.github.com>
2024-05-16 17:16:32 -0500

    feat: :construction: trabajando en el layaout web component

--- src/my-element.js
+++ src/my-element.js
@@ -8,124 +8,54 @@ import viteLogo from '/vite.svg'
  * @slot - This element has a slot
  * @csspart button - The button
  */
-export class MyElement extends LitElement {
-  static get properties() {
-    return {
-      /**
-       * Copy for the read the docs hint.
-       */
-      docsHint: { type: String },
 
-      /**
-       * The number of times the button has been clicked.
-       */
-      count: { type: Number },
-    }
-  }
 
+
+
+export class MyGrid extends LitElement {
   constructor() {
     super()
-    this.docsHint = 'Click on the Vite and Lit logos to learn more'
-    this.count = 0
   }
 
   render() {
     return html`
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-      </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
+    <main>
+        <section class="section_1_menu"></section>
+        <section class="section_2_products"></section>
     `
-  }
-
-  _onClick() {
-    this.count++
   }
 
   static get styles() {
     return css`
-      :host {
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 2rem;
+    main{
+        display: grid;
+        grid-template-rows: repeat(1, 1fr);
+        grid-template-columns: 1fr 4fr;
+        grid-template-areas: 
+            "section_1_menu section_2"
+            "section_1_menu section_2"
+        ;
+    }
+    
+    .section_1_menu{
+        grid-area: section_1_menu;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
         text-align: center;
-      }
-
-      .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.lit:hover {
-        filter: drop-shadow(0 0 2em #325cffaa);
-      }
-
-      .card {
-        padding: 2em;
-      }
-
-      .read-the-docs {
-        color: #888;
-      }
-
-      a {
-        font-weight: 500;
-        color: #646cff;
-        text-decoration: inherit;
-      }
-      a:hover {
-        color: #535bf2;
-      }
-
-      ::slotted(h1) {
-        font-size: 3.2em;
-        line-height: 1.1;
-      }
-
-      button {
-        border-radius: 8px;
-        border: 1px solid transparent;
-        padding: 0.6em 1.2em;
-        font-size: 1em;
-        font-weight: 500;
-        font-family: inherit;
-        background-color: #1a1a1a;
-        cursor: pointer;
-        transition: border-color 0.25s;
-      }
-      button:hover {
-        border-color: #646cff;
-      }
-      button:focus,
-      button:focus-visible {
-        outline: 4px auto -webkit-focus-ring-color;
-      }
-
-      @media (prefers-color-scheme: light) {
-        a:hover {
-          color: #747bff;
-        }
-        button {
-          background-color: #f9f9f9;
-        }
-      }
+    }
+    
+    .section_2_products{
+        grid-area: section_2;
+        background: var(--color-principal);
+        width: 80vw;
+        height: 63em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     `
   }
 }
 
-window.customElements.define('my-element', MyElement)
+window.customElements.define('my-grid', MyGrid)
