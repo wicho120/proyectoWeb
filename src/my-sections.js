b351d2339b31e042868c65aa9a7533c01fd3b4ef
Wicho120 <35779309+wicho120@users.noreply.github.com>
2024-05-20 17:52:08 -0500

    feat: :construction: trabajando en la integracion de botones

--- src/my-sections.js
+++ src/my-sections.js
@@ -2,38 +2,69 @@ import { LitElement, css, html } from 'lit'
 import litLogo from './assets/lit.svg'
 import viteLogo from '/vite.svg'
 
-const getData = async()=>{
-  let res = await fetch("http://localhost:5501/pantalon")
-  let data = await res.json();
-  let dataUpdate = data.map(val =>{
-      return {
-          name: val.nombre,
-          img: val.imagen,
-          price: val.precio,
-          id: val.id 
-      }
-  })
-  return dataUpdate
-}
 
+  const getData = async(types) => {
+    let res = await fetch(`http://localhost:5501/${types}`)
+    let data = await res.json();
+    let dataUpdate = data.map(val =>{
+        return {
+            name: val.nombre,
+            img: val.imagen,
+            price: val.precio,
+            id: val.id 
+        }
+    })
+
+    return dataUpdate
+    
+  
+  }
 
 export class Mysection1 extends LitElement {
   constructor() {
     super();
+    this.data;
+    this.types="abrigo";
   }
+
+  static properties={
+    data: { type: Array },
+  }
+
+  async handleMyEvent(e){
+    
+    if(e.target.textContent === "Abrigos"){
+      this.types = "abrigo";
+    } if (e.target.textContent === "Camisetas"){
+      this.types = "camiseta";
+    } if (e.target.textContent === "Pantalones"){
+      this.types = "pantalon";
+    }
+    this.data = await getData(this.types);
+    this.requestUpdate();
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+    this.data = await getData(this.types);
+  }
+
 
   render() {
     return html`
+
+    <main>
+        <section class="section_1_menu">
           <div class="my-section-1-container">
             <article class="section_1_menu_title">
                 <h1>CampusShop</h1>
             </article>
 
             <article class="section_1_menu_categories">
-                <p><a>Todos los productos</a></p>
-                <p><a>Abrigos</a></p>
-                <p><a>Camisetas</a></p>
-                <p><a>Pantalones</a></p>
+                <p><button @click=${this.handleMyEvent}>Todos los productos</button></p>
+                <p><button @click=${this.handleMyEvent}>Abrigos</button></p>
+                <p><button @click=${this.handleMyEvent}>Camisetas</button></p>
+                <p><button @click=${this.handleMyEvent}>Pantalones</button></p>
             </article>
 
             <article class="section_1_menu_carrito">
@@ -44,128 +75,140 @@ export class Mysection1 extends LitElement {
                 <p>® 2024 Camper</p>
             </article>
           </div>
+        </section>
+
+
+        <section class="section_2_products">
+          <div class="container">
+            <article class="section_2_products_box">
+                ${this.data.map(datas => html`
+                  <div id="contenedor">
+                    <img src = "${datas.img}"alt="">
+                    <div class="info_products">
+                        <div>
+                            <p>${datas.name}</p>
+                            <p>${datas.price}</p>
+                        </div>
+                        <div>
+                            <button>Añadir</button>
+                        </div>
+                    </div>
+                  </div>
+                  `
+                )}
+            </article>
+          </div>
+        </section>
+    </main>
+          
     `
   }
+
 
   static get styles() {
 
 
     return css`
-    .my-section-1-container{
+
+    main{
+      display: grid;
+      grid-template-rows: repeat(1, 1fr);
+      grid-template-columns: 1fr 4fr;
+      grid-template-areas: 
+          "section_1_menu section_2"
+          "section_1_menu section_2"
+      ;
+      background: gray;
+      height: 100vh;
+      width: 100vw;
+  }
+  
+  .section_1_menu{
+      grid-area: section_1_menu;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      text-align: center;
+      height: 100vh;
+  }
+  
+  .section_2_products{
+      grid-area: section_2;
+      background: var(--color-principal);
+      width: 100%;
+      height: 100%;
+  }
+
+  .my-section-1-container{
       height: 100vh;
       width: 100%;
       display: flex;
       flex-direction: column;
       justify-content: center;
 
-    }
+  }
 
-    .section_1_menu article p,a{
+  .section_1_menu article p,a{
         font-size: 1.5em;
         text-decoration: none;
         color: var(--color-bone);
-    }
+  }
 
-    .section_1_menu_carrito img{
+  .section_1_menu_carrito img{
       width: 2.5em;
       height: 2.5em;
   }
     
-    .section_1_menu_title h1{
-        font-size: 2.5em;
-        color: var(--color-bone);
-    }
+  .section_1_menu_title h1{
+      font-size: 2.5em;
+      color: var(--color-bone);
+  }
     
     .section_1_menu_firma p{
-        font-size: 1em;
+      font-size: 1em;
     }
-    `
-  }
-}
 
-export class Mysection2 extends LitElement {
-  constructor() {
-    super();
-    this.data;
-  }
+    .container{
+      display: flex;
+      width: 100%;
+      height: 100%;
+      justify-content: center;
+      align-items: center;
+    }
 
-  static properties={
-    data: { type: Array }
-  }
+    .section_2_products_box{
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      width: 95%;
+      height: 95%;
+      background: #E3DAC9;
+      border-radius: 2em;
+      align-items: center;
+      justify-items: center;
+      overflow-y: scroll;
+    }
 
-  async connectedCallback() {
-    super.connectedCallback();
-    this.data = await getData();
-  }
-
-  render() {
-    return html`
-      <div class="container">
-        <article class="section_2_products_box">
-            ${this.data.map(datas => html`
-              <div id="contenedor">
-                <img src = "${datas.img}"alt="">
-                <div class="info_products">
-                    <div>
-                        <p>${datas.name}</p>
-                        <p>${datas.price}</p>
-                    </div>
-                    <div>
-                        <button>Añadir</button>
-                    </div>
-                </div>
-              </div>
-              `
-            )}
-        </article>
-      </div>
-    `
-          }
-
-  static get styles() {
-
-
-    return css`
-      .container{
-        display: flex;
-        width: 100%;
-        height: 100%;
-        justify-content: center;
-        align-items: center;
-      }
-
-      .section_2_products_box{
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        width: 95%;
-        height: 95%;
-        background: #E3DAC9;
-        border-radius: 2em;
-        align-items: center;
-        justify-items: center;
-        overflow-y: scroll;
-      }
-
-      .section_2_products_box #contenedor{
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        width: 20vw;
-        height: 40vh;
-        background: orange;;
-        margin-bottom: 5em;
-        margin-top: 5em;
-        border-radius: 1em;
-      }
-    
-      .section_2_products_box img{
-        width: 20vw;
-        height: 30vh;
-        object-fit: contain;
-      }
+    .section_2_products_box #contenedor{
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      width: 20vw;
+      height: 40vh;
+      background: orange;;
+      margin-bottom: 5em;
+      margin-top: 5em;
+      border-radius: 1em;
+    }
+  
+    .section_2_products_box img{
+      width: 20vw;
+      height: 30vh;
+      object-fit: contain;
+    }
     `
   }
 }
 
 window.customElements.define('my-section-1', Mysection1)
-window.customElements.define('my-section-2', Mysection2)
+
