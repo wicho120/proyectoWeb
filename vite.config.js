8851ea839e5f8307f874f6e8f8d8d89dd7e4686e
Wicho120 <35779309+wicho120@users.noreply.github.com>
2024-05-24 15:47:31 -0500

    feat: :bug: correcion de la ip y puerto

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
         server: {
                 host:"172.16.101.146",
-                port:5747
+                port:5750
         }
 })
